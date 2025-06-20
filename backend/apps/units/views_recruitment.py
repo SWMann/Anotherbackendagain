a440485aa1351b91acd4a854d38e5250a40a0bfe
SWMann <schaeferwmann@gmail.com>
2025-06-20 12:52:01 +0100

    First Commit

--- backend/apps/units/views_recruitment.py
+++ backend/apps/units/views_recruitment.py
@@ -66,6 +66,7 @@ class RecruitmentStatusViewSet(viewsets.ViewSet):
     @action(detail=True, methods=['get'])
     def platoons(self, request, pk):
         """Get platoons for a specific brigade"""
+        print(pk)
         brigade = get_object_or_404(Unit, pk=pk, unit_type='brigade')
 
         # Get all platoons under this brigade
