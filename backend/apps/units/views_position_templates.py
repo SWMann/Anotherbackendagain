372a630ae284f61436a714249fad08a8b82b0f94
SWMann <schaeferwmann@gmail.com>
2025-06-20 22:35:49 +0100

    First Commit

--- backend/apps/units/views_position_templates.py
+++ backend/apps/units/views_position_templates.py
@@ -3,6 +3,8 @@
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.db import models
+
 from django.shortcuts import get_object_or_404
 from django.db import transaction
 from .models import PositionTemplate, TemplatePosition, Position, Unit
